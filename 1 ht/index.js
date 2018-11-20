47ccb5f85cbc8e37135f5fdec46e4bc5203bc1c2
Артем <kapec_art@mail.ru>
2018-11-20 17:51:02 +0600

    Создал 2 папки

--- 1 ht/index.js
+++ 1 ht/index.js
@@ -1,26 +1,29 @@
-var money;
-var time;
+let money;
+let time;
 
- money=+prompt("Ваш бюджет на месяц?");
- time=prompt("Введите дату в формате YYYY-MM-DD");
- var appData=
- {
-     money:money,
-     timeData:time,
-     expenses:{},
-     optionalExpenses:{},
-     income:{},
-     savings:false,
+money = +prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
+var appData = {
+    money: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: {},
+    savings: false
+};
 
- }
- var ccompulsoryConsumption=prompt("Введите обязательную статью расходов в этом месяце");
- var  total=  +prompt("Во сколько обойдется?");
-
- var expenses={};
- expenses[ccompulsoryConsumption]=total;
- appData.expenses=expenses;
-
- alert("Дневной бюджет:" (appData.money-appData.expenses[ccompulsoryConsumption])/30)
-
-
- 
+for (let i = 0; i < 2; i++) {
+    var ccompulsoryConsumption = prompt(
+        "Введите обязательную статью расходов в этом месяце"
+    );
+    let total = +prompt("Во сколько обойдется?");
+    let expenses = {};
+    expenses[ccompulsoryConsumption] = total;
+    appData.expenses = expenses;
+    alert(
+        "Дневной бюджет:" + (
+            appData.money - appData.expenses[ccompulsoryConsumption]
+        ) / 30
+    );
+}
+console.log(appData);
