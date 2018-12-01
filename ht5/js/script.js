6d4387bc7f5e8845951a9bb0ff43c825803ad073
Артем <kapec_art@mail.ru>
2018-12-01 22:44:43 +0600

    'asdf'

---
+++ ht5/js/script.js
@@ -0,0 +1,44 @@
+let menuItems = document.querySelectorAll(".menu .menu-item");
+let menu = document.querySelector(".menu");
+var numbers = [{
+        name:"Первый пункт",
+        number: 1
+    },
+    {
+        name:"Второй пункт",
+        number: 2
+    },
+    {
+        name:"Третий пункт",
+        number:3
+    },
+    {
+        name:"Четвертый пункт",
+        number: 4
+    },
+    {
+        name:"Пятый пункт",
+        number: 5
+    }
+];
+
+menuItems.forEach(element => 
+    {
+        console.log(element.outerHTML);
+        let number=numbers.find(function(item)
+        {
+            return item.name==element.innerHTML;
+
+        }); 
+
+        number.element=element;
+        
+});
+
+var newElement= menuItems[0].cloneNode(true);
+newElement.innerHTML="Пятый элемент";
+numbers[4].element=newElement;
+console.log(newElement);
+numbers.forEach(element => {
+    menu.append(element.element);
+});
