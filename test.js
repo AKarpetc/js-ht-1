d5dcb323469abdae180a66bc046ac489b7c305c0
AKarpetc <akarpetc@kazzinc.kz>
2018-12-11 16:50:02 +0600

    ht7

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var obj = {
-    test: "Тест",
-    ps: "ps"
+  test: "Тест",
+  ps: "ps"
 };
 obj.test2 = "test2";
 delete obj.test;
 
 function hello() {
-    console.log("Hello World");
+  console.log("Hello World");
 }
 
 var mass = [1, 5, 8, 9, 7, 2, 5, 4];
@@ -14,34 +14,27 @@ var mass = [1, 5, 8, 9, 7, 2, 5, 4];
 mass.sort(compareNum);
 
 function compareNum(a, b) {
-    return a - b;
+  return a - b;
 }
 var divsGrid = document.querySelectorAll("#grid div");
 var grid = document.querySelectorAll("#grid");
 let button = document.getElementById("first");
 let buttons = document.getElementsByTagName("button"),
-    curcits = document.getElementsByClassName("circle"),
-    curcitsAll = document.querySelectorAll("#grid .circle");
+  curcits = document.getElementsByClassName("circle"),
+  curcitsAll = document.querySelectorAll("#grid .circle");
+
+//let timerId = setTimeout(sayHello, 3000);
+//clearTimeout(timerId);
+
+/*function sayHello() {
+    console.log("Hello");
+}
 
 
-curcitsAll.forEach((element, i, heart) => {
-    console.log(element);
-});
+let timerId = setInterval(sayHello, 3000);
+clearTimeout(timerId);*/
 
-console.log(curcitsAll);
-
-button.style.backgroundColor = 'blue';
-
-
-let newdivs = [];
-divsGrid.forEach(element => {
-    let div = document.createElement('button');
-    div.classList.add("circle");
-    element.appendChild(div);
-    newdivs.push(div);
-});
-
-divsGrid[0].removeChild(newdivs[0]);
-
-let newButton = document.createElement('button');
-
+/*let timerId = setTimeout(function log() {
+    console.log("HEllo");
+    setTimeout(log, 2000);
+});*/
