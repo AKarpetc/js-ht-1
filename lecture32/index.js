60049a97fc5e75f4634b0823b2b5852288f68531
AKarpetc <akarpetc@kazzinc.kz>
2018-12-28 17:28:22 +0600

    'lecture32'

---
+++ lecture32/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function shoot(arrow, headshot, fail) {
+    console.log('Вы сделали выстрел');
+    setTimeout(function () {
+        Math.random() > .5 ? headshot({}) : fail('Вы промахнулись')
+    });
+}
+
+shoot({}, function (mark) {
+    console.log('Вы попали в цель!');
+}, function (miss) {
+    console.error(miss);
+});
